4922076e4592e1631fa57af55a03eb06827dbfb8
sangsioco <sangsioco@gmail.com>
2024-05-20 14:42:09 -0700

    larget number

---
+++ LargestSmallest.py
@@ -0,0 +1,16 @@
+# Task 1: Identify the greatest number
+# Prompt the user to enter three numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+
+# Identify the largest number
+if num1 >= num2 and num1 >= num3:
+    largest = num1
+elif num2 >= num1 and num2 >= num3:
+    largest = num2
+else:
+    largest = num3
+
+# Print out the largest number
+print("The largest number is:", largest)
